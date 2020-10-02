32a28ae78d6fecc55637429677e8ae19946688f6
erfolen <dlababy@mail.ru>
2020-10-03 01:22:42 +0300

    init: start project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
 exports.min = function min (array) {
-  return 0;
+  return min.sort((a,b) => a-b).shift();
 }
 
 exports.max = function max (array) {
-  return 0;
+  return min.sort((a,b) => a-b).pop();
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let k = min.reduce((acc, item) => acc + item, 0)
+  return k/min.length;
 }
