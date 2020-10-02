d34969f9671f11bb504ded021042a46416c3d923
erfolen <dlababy@mail.ru>
2020-10-03 01:28:49 +0300

    fix: fixed function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
 
 exports.min = function min (array) {
-  return min.sort((a,b) => a-b).shift();
+  let k;
+
+  if (Array.isArray(min) & min.length !== 0) {
+    k = min.sort((a,b) => a-b).shift();
+  } else {
+    k = 0;
+  }
+  return k;
 }
 
 exports.max = function max (array) {
-  return min.sort((a,b) => a-b).pop();
+  let k;
+
+  if (Array.isArray(min) & min.length !== 0) {
+    k = min.sort((a,b) => a-b).pop();
+  } else {
+    k = 0;
+  }
+  return k; 
 }
 
 exports.avg = function avg (array) {
-  let k = min.reduce((acc, item) => acc + item, 0)
-  return k/min.length;
+  let k;
+
+  if (Array.isArray(min) & min.length !== 0) {
+    k = min.reduce((acc, item) => acc + item, 0);
+    k /= min.length 
+  } else {
+    k = 0;
+  }
+  return k;  
 }
